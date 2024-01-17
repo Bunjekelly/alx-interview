6fe4839e4acfbd521dad42bbdbbe05bfc815067a
Bunje Kelly <kellybunje@gmail.com>
2024-01-17 09:17:32 +0000

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" a method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file"""
+
+def minOperations(n):
+    """function definition"""
+    if n < 2:
+        return 0
+
+    result = 0
+
+    for i in range(2, n + 1):
+        while n % i == 0:
+            result += i
+            n = n // i
+
+        if n == 1:
+            break
+
+    return result
