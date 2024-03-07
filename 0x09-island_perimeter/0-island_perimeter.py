edd3e0c3e6f06793bf8d87b72ba5afebdcb4a854
Bunje Kelly <kellybunje@gmail.com>
2024-03-07 12:53:09 +0000

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,6 +3,7 @@
 """a function def island_perimeter(grid): that returns
 the perimeter of the island described in grid"""
 
+
 def island_perimeter(grid):
     """a function def island_perimeter(grid): that returns
     the perimeter of the island described in grid"""
